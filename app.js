cfd9b82e3b6242b26a8feee0b5dba7b0dece3140
Harshit Pant <hrshtpnt@gmail.com>
2018-04-21 10:36:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var myApp = angular.module('myApp',['ngRoute']);
 myApp.config(function($routeProvider){
 
 	$routeProvider
-	
 		.when("/", {
 			templateUrl:"view/main.html",
 			controller:"homepageController"
@@ -11,24 +10,5 @@ myApp.config(function($routeProvider){
 		.when("/portfolio", {
 			templateUrl:"view/portfolio.html",
 			controller:"portfolioController"
-		})
-		.when("/blog", {
-			templateUrl: "view/blog.html",
-			controller: "blogController"
-		})
-		.when("/admin",{
-				resolve: { //this is called conditional routing
-					"check" : function($location, $rootScope){
-						if(!$rootScope.sessiondata){
-							$location.path("/");
-						}
-					}
-				},
-				templateUrl: "view/admin.php",
-				controller: "adminController"
-		})
-		.when("/loginpage", {
-			templateUrl: "view/loginpage.html",
-			controller: "postController"
 		}).otherwise("/")
 });
